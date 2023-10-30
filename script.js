1f1175e164d8e70469cfcf14dd3e52ca2137a5f7
Ricky Waldt <121530629+rickyharvywaldt@users.noreply.github.com>
2023-10-30 20:38:27 +0700

    add tooltip on hover with hardcoded value

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ let i = 0;
 
 while (i < 364) {
   const para = document.createElement("div");
+  const tooltip = document.createElement("span");
+  const tooltipText = document.createTextNode("No activity on February 14th.");
+  tooltip.appendChild(tooltipText);
 
   para.classList.add("mystyle");
+  tooltip.classList.add("tooltip-text");
 
   const element = document.getElementById("wrapper");
   element.appendChild(para);
+  para.appendChild(tooltip);
   i++;
 }
