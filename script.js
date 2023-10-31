5b0838d4b2ee5e97cd0271ef42cd00637798bf81
Ricky Waldt <121530629+rickyharvywaldt@users.noreply.github.com>
2023-10-31 21:21:10 +0700

    add scroll and fixed width

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ btn.addEventListener("click", () => {
   tooltips.forEach((tooltip) => {
     tooltip.textContent = dateTxt;
   });
-  const days = document.querySelectorAll(".mystyle:not(:nth-child(n + 364))");
+  const days = document.querySelectorAll(".mystyle:not(:nth-child(n + 361))");
   days.forEach((day) => {
     if (c < 5) {
       day.style.backgroundColor = colors.a;
