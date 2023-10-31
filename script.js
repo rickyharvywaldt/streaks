1110c51892c8812efb949a39387395e5e9a92856
Ricky Waldt <121530629+rickyharvywaldt@users.noreply.github.com>
2023-10-31 11:54:36 +0700

    change date to current date

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+const month = new Date().toLocaleString("default", { month: "long" });
+const d = new Date().toString().slice(8, 10);
+
+const nthNumber = (number) => {
+  if (number > 3 && number < 21) return "th";
+  switch (number % 10) {
+    case 1:
+      return "st";
+    case 2:
+      return "nd";
+    case 3:
+      return "rd";
+    default:
+      return "th";
+  }
+};
+
+const dateTxt = `0 activity on ${month} ${d}${nthNumber(d)}`;
+console.log(dateTxt);
+
 let i = 0;
 
 while (i < 371) {
   const para = document.createElement("div");
   const tooltip = document.createElement("span");
-  const tooltipText = document.createTextNode("No activity on February 14th.");
+  const tooltipText = document.createTextNode(dateTxt);
   tooltip.appendChild(tooltipText);
 
   para.classList.add("mystyle");
